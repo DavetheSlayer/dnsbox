8e64a493389318e46570d625a69bc16816a2341f
Burak Budanur <burakbudanur@gmail.com>
2016-09-16 18:08:07 +0200

    python version ups

--- python/dnsbox.py
+++ python/dnsbox.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import pi, sin, cos, exp
 from numpy.fft import fftn, ifftn
+import pickle
 
 # Parameters:
 global Nx, Ny, Nz, Nt, alphax, alphay, alphaz,\
@@ -13,14 +14,13 @@ global Nx, Ny, Nz, Nt, alphax, alphay, alphaz,\
 Nx = 64            # Spatial discretization
 Ny = 64            # Spatial discretization
 Nz = 64            # Spatial discretization
-Nt = 10            # Maximum number of time stepes
 iSaveRate1 = 10    # Save state files every iSaveRate1 time steps
 iSaveRate2 = 1     # Save state files every iSaveRate2 time steps
 alphax = 1.0       # Base wave numbers
 alphay = 1.0       # Base wave numbers
 alphaz = 1.0       # Base wave numbers
-nu = 1.0           # Viscosity           0.2
-Q = 0.0            # Forcing amplitude   0.5
+nu = 0.2           # Viscosity           0.2
+Q = 0.5            # Forcing amplitude   0.5
 Deltak = 1.0       # k-window for shell averaging
 CourantMin = 0.15  # Minimum Courant number
 CourantMax = 0.2   # Maximum Courant number
@@ -28,8 +28,8 @@ tStepMax = 0.01    # Maximum time step
 
 # Logicals
 spherical = True   # Spherical truncation
-analytic = True    # Analytical initial condition
-random = False     # Random initial condition
+analytic = False    # Analytical initial condition
+random = True      # Random initial condition
 tStepFix = True    # Fixed time-step simulation, if true
 
 # Random initial field generation parameters:
@@ -52,12 +52,12 @@ def setGrid():
                  dtype='float')
     z = np.array([i * (Lz / Nz) for i in range(-Nz / 2, 1 + Nz / 2)],
                  dtype='float')
-    kx = alphax * np.array([1j * n for n in list(range(0, Nx / 2)) + [0]
-                                          + list(range(-Nx / 2 + 1, 0))])
-    ky = alphay * np.array([1j * n for n in list(range(0, Ny / 2)) + [0]
-                                          + list(range(-Ny / 2 + 1, 0))])
-    kz = alphaz * np.array([1j * n for n in list(range(0, Nz / 2)) + [0]
-                                          + list(range(-Nz / 2 + 1, 0))])
+    kx = alphax * np.array([n for n in list(range(0, Nx / 2)) + [0]
+                                     + list(range(-Nx / 2 + 1, 0))])
+    ky = alphay * np.array([n for n in list(range(0, Ny / 2)) + [0]
+                                     + list(range(-Ny / 2 + 1, 0))])
+    kz = alphaz * np.array([n for n in list(range(0, Nz / 2)) + [0]
+                                     + list(range(-Nz / 2 + 1, 0))])
 
     return
 
@@ -69,9 +69,9 @@ def alloc():
     global u, v, w, ux, uy, uz, vx, vy, vz, wx, wy, wz,\
            omegax, omegay, omegaz,\
            uhat, vhat, what, phat, nonlinuhat, nonlinvhat, nonlinwhat,\
-           xx, yy, zz, kxm, kym, kzm, k2xm, k2ym, k2zm,\
+           xx, yy, zz, kxm, kym, kzm, kkm, \
            temp, temphat, utemp, vtemp, wtemp, uhattemp, vhattemp, whattemp,\
-           intFact  # , uhatold, vhatold, whatold
+           intFact
 
     u = np.zeros((Nx, Ny, Nz), dtype='float')
     v = np.zeros((Nx, Ny, Nz), dtype='float')
@@ -104,18 +104,13 @@ def alloc():
     kym = np.zeros((Nx, Ny, Nz), dtype='complex')
     kzm = np.zeros((Nx, Ny, Nz), dtype='complex')
 
-    k2xm = np.zeros((Nx, Ny, Nz), dtype='float')
-    k2ym = np.zeros((Nx, Ny, Nz), dtype='float')
-    k2zm = np.zeros((Nx, Ny, Nz), dtype='float')
+    kkm = np.zeros((Nx, Ny, Nz), dtype='float')
 
     intFact = np.zeros((Nx, Ny, Nz), dtype='float')
     temphat = np.zeros((Nx, Ny, Nz), dtype='complex')
     uhattemp = np.zeros((Nx, Ny, Nz), dtype='complex')
     vhattemp = np.zeros((Nx, Ny, Nz), dtype='complex')
     whattemp = np.zeros((Nx, Ny, Nz), dtype='complex')
-    #uhatold = np.zeros((Nx, Ny, Nz), dtype='complex')
-    #vhatold = np.zeros((Nx, Ny, Nz), dtype='complex')
-    #whatold = np.zeros((Nx, Ny, Nz), dtype='complex')
     utemp = np.zeros((Nx, Ny, Nz), dtype='float')
     vtemp = np.zeros((Nx, Ny, Nz), dtype='float')
     wtemp = np.zeros((Nx, Ny, Nz), dtype='float')
@@ -145,12 +140,36 @@ def uhat2u():
     return
 
 
+def project():
+    """
+    Project state to make it divergence free
+    """
+    global uhat, vhat, what, uhattemp, vhattemp, whattemp
+    uhattemp = np.copy(uhat)
+    vhattemp = np.copy(vhat)
+    whattemp = np.copy(what)
+
+    uhat = uhattemp - kxm * kxm * uhattemp / kkm \
+                    - kxm * kym * vhattemp / kkm \
+                    - kxm * kzm * whattemp / kkm
+
+    vhat = vhattemp - kym * kxm * uhattemp / kkm \
+                    - kym * kym * vhattemp / kkm \
+                    - kym * kzm * whattemp / kkm
+
+    what = whattemp - kzm * kxm * uhattemp / kkm \
+                    - kzm * kym * vhattemp / kkm \
+                    - kzm * kzm * whattemp / kkm
+
+    return
+
+
 def init():
     """
     Initiate simulation
     """
-    global t, lamb, sk, sl, sm, A, kxm, kym, kzm, k2xm, k2ym, k2zm, xx, yy, zz,\
-           u, v, w, uhat, vhat, what
+    global t, lamb, sk, sl, sm, A, kxm, kym, kzm, xx, yy, zz,\
+           u, v, w, uhat, vhat, what, saveCount
     print('Initiating variables, grids, and fields')
     t = 0.0
     setGrid()
@@ -162,13 +181,15 @@ def init():
                 kym[i, j, k] = ky[j]                                  # lint:ok
                 kzm[i, j, k] = kz[k]                                  # lint:ok
 
-                k2xm[i, j, k] = np.real(kx[i] ** 2)                   # lint:ok
-                k2ym[i, j, k] = np.real(ky[i] ** 2)                   # lint:ok
-                k2zm[i, j, k] = np.real(kz[i] ** 2)                   # lint:ok
-
                 xx[i, j, k] = x[i]                                    # lint:ok
-                yy[i, j, k] = y[i]                                    # lint:ok
-                zz[i, j, k] = z[i]                                    # lint:ok
+                yy[i, j, k] = y[j]                                    # lint:ok
+                zz[i, j, k] = z[k]                                    # lint:ok
+                if kx[i] == 0 and ky[j] == 0 and kz[k] == 0:
+                    kkm[i, j, k] = 1.0e-13  # to avoid div by zeros
+                else:
+                    kkm[i, j, k] = np.real(kx[i] ** 2) \
+                                 + np.real(ky[j] ** 2) \
+                                 + np.real(kz[k] ** 2)  # k^2
 
     if analytic:
         # Initiate simulation from an analytical solution for test. See
@@ -180,27 +201,50 @@ def init():
         lamb = np.sqrt(sk ** 2 + sl ** 2 + sm ** 2)
         A = 1.0
         print('generating initial condition based on analytical solution')
-        u = (-A / (sk ** 2 + sl ** 2)) \
+        u = (- A / (sk ** 2 + sl ** 2)) \
           * (lamb * sl * cos(sk * xx) * sin(sl * yy) * sin(sm * zz)
            + sm * sk * sin(sk * xx) * cos(sl * yy) * cos(sm * zz)) \
-          * exp(- lamb ** 2 * t * nu)                                 # lint:ok
+          * exp(-1.0 * (lamb ** 2) * nu * t)                          # lint:ok
         v = (A / (sk ** 2 + sl ** 2)) \
           * (lamb * sk * sin(sk * xx) * cos(sl * yy) * sin(sm * zz)
            - sm * sl * cos(sk * xx) * sin(sl * yy) * cos(sm * zz)) \
-          * exp(- lamb ** 2 * t * nu)                                 # lint:ok
+          * exp(-1.0 * (lamb ** 2) * nu * t)                          # lint:ok
         w = A * cos(sk * xx) * cos(sl * yy) * sin(sm * zz) \
-          * exp(- lamb ** 2 * t * nu)
-        # del xx, yy, zz                                                # lint:ok
+          * exp(-1.0 * (lamb ** 2) * nu * t)                          # lint:ok
+        # del xx, yy, zz                                              # lint:ok
     elif random:
+        # Initiation method: Rosales & Meneveau (2005), Phys. Fluids. eq.9
         print('random initial field')
-        print('not implemented yet')
-        return
+        uhat = np.sqrt(4 * (uzero ** 2) * kkm * (kzero ** (-5))
+                     * (pi ** (-3.0 / 2.0)) * (2.0 ** (-0.5))
+                     * exp(-2.0 * kkm / (kzero ** 2))) \
+               * exp(1j * (np.random.rand(Nx, Ny, Nz) - 0.5) * 2 * pi) \
+               * Nx * Ny * Nz
+        vhat = np.sqrt(4 * (uzero ** 2) * kkm * (kzero ** (-5))
+                     * (pi ** (-3.0 / 2.0)) * (2.0 ** (-0.5))
+                     * exp(-2.0 * kkm / (kzero ** 2))) \
+               * exp(1j * (np.random.rand(Nx, Ny, Nz) - 0.5) * 2 * pi) \
+               * Nx * Ny * Nz
+        what = np.sqrt(4 * (uzero ** 2) * kkm * (kzero ** (-5))
+                     * (pi ** (-3.0 / 2.0)) * (2.0 ** (-0.5))
+                     * exp(-2.0 * kkm / (kzero ** 2))) \
+               * exp(1j * (np.random.rand(Nx, Ny, Nz) - 0.5) * 2 * pi) \
+               * Nx * Ny * Nz
+
+        uhat[0, 0, 0] = 0.0
+        vhat[0, 0, 0] = 0.0
+        what[0, 0, 0] = 0.0
+
+        project()
+        uhat2u()
+
     else:
         print('read initial field from file')
         print('not implemented yet')
         return
 
     u2uhat()  # Fourier transform initial condition
+    saveCount = 0
 
     return
 
@@ -213,8 +257,9 @@ def setTimeStep(tStep=tStepMax):
         print('Resetting time step')
         print('not implemented yet')
         return
-
-    intFact = exp((nu * (k2xm + k2ym + k2zm) + Q) * dt)
+    print('Set the time step to, dt = ', dt)
+    #intFact = exp((- nu * (k2xm + k2ym + k2zm) + Q) * dt)
+    intFact = exp((- nu * kkm + Q) * dt)
 
     return
 
@@ -224,25 +269,26 @@ def derivatives():
     Compute space derivatives from (u,v,w)hattemp
     """
     global ux, uy, uz, vx, vy, vz, wx, wy, wz
-    temphat = kxm * uhattemp
+
+    temphat = 1j * kxm * uhattemp
     ux = np.real(ifftn(temphat))
-    temphat = kym * uhattemp
+    temphat = 1j * kym * uhattemp
     uy = np.real(ifftn(temphat))
-    temphat = kzm * uhattemp
+    temphat = 1j * kzm * uhattemp
     uz = np.real(ifftn(temphat))
 
-    temphat = kxm * vhattemp
+    temphat = 1j * kxm * vhattemp
     vx = np.real(ifftn(temphat))
-    temphat = kym * vhattemp
+    temphat = 1j * kym * vhattemp
     vy = np.real(ifftn(temphat))
-    temphat = kzm * vhattemp
+    temphat = 1j * kzm * vhattemp
     vz = np.real(ifftn(temphat))
 
-    temphat = kxm * whattemp
+    temphat = 1j * kxm * whattemp
     wx = np.real(ifftn(temphat))
-    temphat = kym * whattemp
+    temphat = 1j * kym * whattemp
     wy = np.real(ifftn(temphat))
-    temphat = kzm * whattemp
+    temphat = 1j * kzm * whattemp
     wz = np.real(ifftn(temphat))
 
     return
@@ -252,7 +298,8 @@ def nonLinear():
     """
     Compute nonlinear term for (u,v,w)hattemp
     """
-    global utemp, vtemp, wtemp, nonlinuhat, nonlinvhat, nonlinwhat, temp, phat
+    global nonlinuhat, nonlinvhat, nonlinwhat, phat
+
     derivatives()  # compute derivatives
     utemp = np.real(ifftn(uhattemp))
     vtemp = np.real(ifftn(vhattemp))
@@ -269,21 +316,20 @@ def nonLinear():
     nonlinwhat = fftn(temp)
 
     # Pressure
-    phat = -1.0 * (kxm * nonlinuhat + kym * nonlinvhat + kzm * nonlinwhat) \
-           / (k2xm + k2ym + k2zm + 1.0e-13)
+    phat = 1j * (kxm * nonlinuhat + kym * nonlinvhat + kzm * nonlinwhat) / kkm
 
-    nonlinuhat = - nonlinuhat - kxm * phat
-    nonlinvhat = - nonlinvhat - kym * phat
-    nonlinwhat = - nonlinuhat - kzm * phat
+    nonlinuhat = - nonlinuhat - 1j * kxm * phat
+    nonlinvhat = - nonlinvhat - 1j * kym * phat
+    nonlinwhat = - nonlinwhat - 1j * kzm * phat
 
     return
 
 
-def timeStep():
+def timeStep(Nt):
     global u, v, w, uhat, vhat, what, uhattemp, vhattemp, whattemp, t
     print('Time stepping')
 
-    for n in range(1, Nt + 1):
+    for n in range(Nt):
 
         uhattemp = np.copy(uhat)
         vhattemp = np.copy(vhat)
@@ -315,21 +361,70 @@ def timeStep():
     return
 
 
+def Ekinetic():
+    """
+    Compute kinetic energy
+    """
+    Ekin = np.real(np.sum(np.conjugate(uhat) * uhat
+                        + np.conjugate(vhat) * vhat
+                        + np.conjugate(what) * what)) / (Nx * Ny * Nz) ** 2
+    return Ekin
+
+
 def checkError():
 
-    uErrorMax = np.max((-A / (sk ** 2 + sl ** 2)) \
-              * (lamb * sl * cos(sk * xx) * sin(sl * yy) * sin(sm * zz)
-                + sm * sk * sin(sk * xx) * cos(sl * yy) * cos(sm * zz)) \
-              * exp(- lamb ** 2 * t * nu) - u)                        # lint:ok
+    print('t = ', t)
+
+    uErrorMax = np.max((- A / (sk ** 2 + sl ** 2)) \
+                     * (lamb * sl * cos(sk * xx) * sin(sl * yy) * sin(sm * zz)
+                       + sm * sk * sin(sk * xx) * cos(sl * yy) * cos(sm * zz)) \
+                     * exp(-1.0 * (lamb ** 2) * nu * t) - u)          # lint:ok
 
     vErrorMax = np.max((A / (sk ** 2 + sl ** 2)) \
                      * (lamb * sk * sin(sk * xx) * cos(sl * yy) * sin(sm * zz)
                        - sm * sl * cos(sk * xx) * sin(sl * yy) * cos(sm * zz)) \
-                     * exp(- lamb ** 2 * t * nu) - v)
+                     * exp(-1.0 * (lamb ** 2) * nu * t) - v)          # lint:ok
 
     wErrorMax = np.max(A * cos(sk * xx) * cos(sl * yy) * sin(sm * zz) \
-                     * exp(- lamb ** 2 * t * nu) - w)
+                     * exp(-1.0 * (lamb ** 2) * nu * t) - w)          # lint:ok
 
     print('Maximum errors at the final time step are: ')
     print(uErrorMax, vErrorMax, wErrorMax)
+    return
+
+
+def saveState():
+
+    global saveCount
+    f = open('state'+str(saveCount).zfill(4)+'.pkl', 'wb')
+    pickle.dump([u, v, w], f)
+    f.close()
+    saveCount = saveCount + 1
+    return
+
+
+def loadState(stateName):
+
+    global u, v, w
+    f = open(stateName, 'rb')
+    u, v, w = pickle.load(f)
+    f.close()
+    return
+
+
+def saveStats():
+    """
+    Save flow statistics
+    """
+    k = Ekinetic()
+    D = Dissipation()
+    P = 2 * Q * Ekin
+    C = Courant()
+    div = divMax()
+
+    f=open('stats.dat' ,'ab')
+    f.write('%11.9f %11.9f %11.9f %11.9f %11.9f %11.9f\n' %
+            (db.t,  k,      D,     P,     C,     div))
+    f.close()
+
     return
---
+++ python/test.py
@@ -0,0 +1,16 @@
+import dnsbox as db
+import numpy as np
+db.init()
+db.setTimeStep(0.001)
+db.saveState()
+
+for n in range(10000):
+    db.timeStep(10)
+    Ekin = db.Ekinetic()
+    print ('t = ', db.t)
+    print ('Ekin =', Ekin)
+
+    if n == 5:
+        db.saveState()
+
+db.checkError()
